82ff07b4d5bacaeefaf4acd3abc848eb198f14e0
Andrew Ho <87453706+anmho@users.noreply.github.com>
2024-09-14 02:49:02 -0700

    aws lambda go

--- lib/aws-lambda-go-stack.ts
+++ lib/aws-lambda-go-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import * as go from '@aws-cdk/aws-lambda-go-alpha';
 
 export class AwsLambdaGoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const gateway = new RestApi(this, 'myGateway', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: ['*'],
+        allowMethods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT'],
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsLambdaGoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const main = new go.GoFunction(this, 'main', {
+      entry: 'lambdas',
+    });
+    const integration = new LambdaIntegration(main);
+    const testResource = gateway.root.addResource('test');
+    testResource.addMethod('GET', integration);
   }
 }
